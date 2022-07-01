0f637e5d1f7948ebeca1aa18c0fc400aacb9a218
Andrei <andreipenskiy@gmail.com>
2022-07-01 14:17:28 +0300

    done

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,7 +1,10 @@
 const updateSubscription = require("./updateSubscription");
 const getCurrent = require("./getCurrent");
+const updateAvatar = require("./updateAvatar")
 
 module.exports = {
   getCurrent,
   updateSubscription,
+  updateAvatar,
+
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { users: ctrl } = require("../../controllers");
-const { validation, auth, ctrlWrapper } = require("../../middlewares");
+const { validation, auth, ctrlWrapper, upload } = require("../../middlewares");
 const { joiSubscriptionSchema } = require("../../models/user");
 
 const router = express.Router();
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 router.patch("/", auth, validation(joiSubscriptionSchema), ctrlWrapper(ctrl.updateSubscription));
+router.patch("/avatars", auth, upload.single("avatar"), ctrlWrapper(ctrl.updateAvatar));
 
 module.exports = router;
